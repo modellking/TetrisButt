46c138142f2e896203dcd41338d7391d16094826
modellking <simon.janssen@live.de>
2021-06-13 22:22:42 +0200

    GMTK21
    ! Collision shapes are off?
    ! Interaction with other island
    ! KI Island movement
    !! AI Entities
    !! Menu
    !! Sound emitter

--- Island.gd
+++ Island.gd
@@ -1,10 +1,21 @@
 extends KinematicBody2D
 
-onready var shapesFactory = get_node("/root/Game/tetrisShapes")
+onready var shapesFactory = $"/root/Game/tetrisShapes"
+var shapeLUT = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_child(shapesFactory.getShape())
+	var col = $"./colider";
+	var newShape = shapesFactory.getShape()
+	col.add_child(newShape)
+	var poly = PoolVector2Array();
+	# collect polygons
+	for shapeInst in col.get_children():
+		if !shapeLUT.has(shapeInst):
+			shapeLUT[shapeInst] = shapeInst.get_node("./Polygon2D");
+		for point in shapeLUT[shapeInst].polygon:
+			poly.append(point)
+	col.polygon = poly
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Player.gd
+++ Player.gd
@@ -3,18 +3,40 @@ extends Node2D
 var velocity = Vector2.ZERO
 var islands
 var pl_island
+var move_step = true
+var delta_since_step = 0
+var input_vector = Vector2.ZERO
+var secPerStep = 0.5;
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var cand = Vector2.ZERO;
+	cand.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
+	cand.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
 	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+	if Input.get_action_strength('ui_speed'):
+		secPerStep = 0.08
 	else:
-		velocity = Vector2.ZERO
+		secPerStep = 0.5;
 	
-	pl_island.move_and_collide(velocity * delta * 1000)
+	if cand != Vector2.ZERO:
+		input_vector = cand;
+	
+	if move_step:
+		delta_since_step = 0;
+		move_step = false;
+		
+		velocity = input_vector.normalized()
+		
+		velocity *= 32
+		
+		var collide = pl_island.move_and_collide(velocity, true, true, true)
+		if collide:
+			var other_island = collide.collider
+		else:
+			pl_island.position += velocity;
+	else:
+		move_step = delta_since_step > secPerStep
+		delta_since_step += delta
 
 
 # Called when the node enters the scene tree for the first time.
